da0f4c65d0755a7ba239c139c4fb540c6588568b
Andreas Kloeckner <inform@tiker.net>
2015-09-19 18:47:49 -0500

    Incorporate machine name in summary script

--- scripts/inner-outer-summary.py
+++ scripts/inner-outer-summary.py
@@ -1,8 +1,8 @@
-p("select rp.cl_platform as cl_plat, rp.sd as sd, rp.deg as deg, rp.prec as prec, "
+p("select state_machine_name as machine, rp.cl_platform as cl_plat, rp.sd as sd, rp.deg as deg, rp.prec as prec, "
         "min(res.elapsed_wall::float) as min, "
         "max(res.elapsed_wall::float) as max, "
         "max(res.elapsed_wall::float)/min(res.elapsed_wall::float) as factor, "
         "count(*) from run "
         "where '[\"andreas\"]'::jsonb <@ rp..tags and state='complete' "
-        "group by rp.cl_platform, rp.sd, rp.deg, rp.prec "
-        "order by rp.sd, rp.deg, rp.prec, rp.cl_platform;")
+        "group by state_machine_name, rp.cl_platform, rp.sd, rp.deg, rp.prec "
+        "order by rp.sd, rp.deg, rp.prec, state_machine_name, rp.cl_platform;")
